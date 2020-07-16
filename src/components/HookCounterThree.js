5f44e48d9ee3031a5e96e8f17682161e27b3f256
jeevanthask <jeevanthanandika@gmail.com>
2020-07-16 10:30:49 +0530

    upgrade

--- src/components/HookCounterThree.js
+++ src/components/HookCounterThree.js
@@ -6,8 +6,8 @@ function HookCounterThree() {
 
     return (
         <form>
-            <input type="text" value={name.firstName} onChange={e => setName({firstName: e.target.value})}/>
-            <input type="text" value={name.lastName} onChange={e => setName({lastName: e.target.value})}/>
+            <input type="text" value={name.firstName} onChange={e => setName({...name, firstName: e.target.value})}/>
+            <input type="text" value={name.lastName} onChange={e => setName({...name, lastName: e.target.value})}/>
 
             <h2>your first name is {name.firstName}</h2>
             <h2>Your Last name is {name.lastName}</h2>
