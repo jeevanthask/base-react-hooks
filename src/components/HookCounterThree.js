df6374ea7a6b50df9b3995f5b030f07e1b29a16a
jeevanthask <jeevanthanandika@gmail.com>
2020-07-16 10:23:11 +0530

    upgrade

---
+++ src/components/HookCounterThree.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+
+function HookCounterThree() {
+
+    const [name, setName] = useState({firstName: '', lastName: ''})
+
+    return (
+        <form>
+            <input type="text" value={name.firstName} onChange={e => setName({firstName: e.target.value})}/>
+            <input type="text" value={name.lastName} onChange={e => setName({lastName: e.target.value})}/>
+
+            <h2>your first name is {name.firstName}</h2>
+            <h2>Your Last name is {name.lastName}</h2>
+
+            <h2>{JSON.stringify(name)}</h2>
+        </form>
+    )
+}
+
+export default HookCounterThree
