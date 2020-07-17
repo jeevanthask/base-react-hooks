cc09173a660a7f130217a3111f73c05082c2ea59
jeevanthask <jeevanthanandika@gmail.com>
2020-07-17 08:21:24 +0530

    upgrade

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import HookCounter from "./components/HookCounter";
 import HookCounterTwo from "./components/HookCounterTwo";
 import HookCounterThree from "./components/HookCounterThree";
 import HookCounterFour from "./components/HookCounterFour";
+import HookCounterFive from "./components/HookCounterFive";
 
 function App() {
     return (
         <div className="App">
-            <HookCounterFour/>
+            <HookCounterFive/>
         </div>
     );
 }
---
+++ src/components/HookCounterFive.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HookCounterFive() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default HookCounterFive
