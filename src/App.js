678f80155b51c9c2462d68c225b9d58c3f38abb2
jeevanthask <jeevanthanandika@gmail.com>
2020-07-18 11:26:49 +0530

    upgrade

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import HookCounterTwo from "./components/HookCounterTwo";
 import HookCounterThree from "./components/HookCounterThree";
 import HookCounterFour from "./components/HookCounterFour";
 import HookCounterFive from "./components/HookCounterFive";
+import ComponentC from "./components/contexthooks/componentc";
+
+export const UserContext = React.createContext()
 
 function App() {
-
-
-
     return (
         <div className="App">
-            <HookCounterFive/>
+
+            <UserContext.Provider value={'nandika sois'}>
+                <ComponentC/>
+            </UserContext.Provider>
+
         </div>
     );
 }
